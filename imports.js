43e644958e30c602b405dcbddb0c148b2d6eb591
cnra <roser137@hotmail.com>
2021-11-10 16:29:37 +0300

    alternatif imports

--- imports.js
+++ imports.js
@@ -1,5 +1,10 @@
-var f1 = require('./inc/functions1');
-var f2 = require('./inc/functions2');
+// var f1 = require('./inc/functions1');
+// var f2 = require('./inc/functions2');
 
 
-module.exports = {...f1, ...f2};
+// module.exports = {...f1, ...f2};
+
+module.exports = {
+    ...require('./inc/functions1'),
+    ...require('./inc/functions2')
+}
