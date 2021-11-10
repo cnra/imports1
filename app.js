d97bd6a13bb621e9d8813996138c929721b66d4c
cnra <roser137@hotmail.com>
2021-11-10 16:26:40 +0300

    kismi import

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var { fun1, fun1b, fun2b } = require('./imports');
 var imports = require('./imports');
 
 imports.fun1();
@@ -6,3 +7,9 @@ imports.fun1c();
 imports.fun1d();
 imports.fun2a();
 imports.fun2b();
+
+console.log("----")
+
+fun1()
+fun1b()
+fun2b()
